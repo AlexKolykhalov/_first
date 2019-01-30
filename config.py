15a576a89fcd7e269ed6226c5c34d55b4697091c
Колыхалов Алексей Викторович <C:\Users\asu7\AppData\Roaming\The Bat!>
2019-01-30 13:00:37 +0300

    we

--- config.py
+++ config.py
@@ -29,4 +29,4 @@ class Config(object):
     ELASTICSEARCH_URL = 'http://localhost:9200'
     # celery
     CELERY_BROKER_URL = 'redis://localhost:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0' 
