8f9bce2c2757259670cd991431e8795cb592b3ce
Колыхалов Алексей Викторович <C:\Users\asu7\AppData\Roaming\The Bat!>
2019-02-01 16:47:33 +0300

    chp 23

--- app/__init__.py
+++ app/__init__.py
@@ -55,6 +55,9 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
     class SSLSMTPHandler(SMTPHandler):
         def emit(self, record):
             """
